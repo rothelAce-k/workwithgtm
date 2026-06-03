fc98f8af34e76d2ed4596f535d000e912e077208
rothelAce-k <maxrothel881@gmail.com>
2026-06-03 06:02:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-// @lovable.dev/vite-tanstack-config already includes the following — do NOT add them manually
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    nitro: {
-      preset: "vercel",
-    },
-
-    server: {
-      entry: "server",
-    },
+    ssr: false,
   },
 });
